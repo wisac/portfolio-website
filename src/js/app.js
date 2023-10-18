152f1c6e18ed1bed0ade1ed4bce0581ca9f55ba9
Isaac Wilson <iswilson.dev@gmail.com>
2023-10-18 16:42:26 +0000

    feat: add about me card

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,11 @@
 import { init as controllerInit } from "./controller.js";
 
 controllerInit();
+
+
+if(module.hot) {
+    module.hot.accept("./controller.js", () => {
+        console.log("HMR - controller.js");
+        controllerInit();
+    });
+}
