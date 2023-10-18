4a6c33379b5d564b10033ee3738ca434ee938e27
Isaac Wilson <iswilson.dev@gmail.com>
2023-10-18 20:42:23 +0000

    feat: add eduction info

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,8 @@ import { init as controllerInit } from "./controller.js";
 
 controllerInit();
 
-
-if(module.hot) {
-    module.hot.accept("./controller.js", () => {
-        console.log("HMR - controller.js");
-        controllerInit();
-    });
+if (module.hot) {
+   module.hot.accept("./controller.js", () => {
+      controllerInit();
+   });
 }
