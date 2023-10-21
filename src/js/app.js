04eacc084426fbd3d11ecbd1bf9c6afcca7e8260
Isaac Wilson <iswilson.dev@gmail.com>
2023-10-21 20:45:06 +0000

    feat: add service and contact sections

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,8 @@ import { init as controllerInit } from "./controller.js";
 
 controllerInit();
 
-if (module.hot) {
-   module.hot.accept("./controller.js", () => {
-      controllerInit();
-   });
-}
+// if (module.hot) {
+//    module.hot.accept("./controller.js", () => {
+//       controllerInit();
+//    });
+// }
