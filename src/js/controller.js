107bd4fb70b9aae834743f2bed453d33e691fe2c
Isaac Wilson <iswilson.dev@gmail.com>
2023-10-21 22:37:41 +0000

    feat: add light mode

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,19 +1,34 @@
-import { View,navigators,navigatorsContainer } from "./navigatorView.js";
+import { View, navigators, navigatorsContainer } from "./navigatorView.js";
 
 // handle click event on navigators
-function navClickHandler (e) {
+function navClickHandler(e) {
    e.preventDefault();
    const target = e.target.closest(".navigator");
    const activeSection = +target.dataset.section;
    if (!target) return;
    View.render(activeSection);
    console.log("clicked");
-
 }
 
+let nightMode = false;
 function init() {
    navigatorsContainer.addEventListener("click", navClickHandler);
-}
+   document.querySelector(".theme-btn").addEventListener("click", () => {
+      document.body.classList.toggle("light-mode");
+      let day = document.querySelector(".day");
+      let night = document.querySelector(".night");
 
+      if (nightMode) {
+         night.style.display = "none";
+         day.style.display = "inline-block";
+         nightMode = false;
+      } else {
+         night.style.display = "inline-block";
+         day.style.display = "none";
+         nightMode = true;
+      }
+
+   });
+}
 
 export { init };
